03d70c873e25af284cbdca74964901ceaa488a47
Eric Michalsen <eric.michalsen@pantheon.io>
2022-04-19 14:02:42 -0400

    first

---
+++ wpcfm_config.php
@@ -0,0 +1,12 @@
+<?php
+
+add_filter( 'wpcfm_config_dir', function($dir) {
+	return $_ENV['DOCUMENT_ROOT'] . '/private/config/dev';
+  }, 10 );
+
+
+  $config_dir = untrailingslashit($_ENV['DOCROOT']) . '/private/config';
+  $config_url = home_url() . '/private/config';
+
+define( 'WPCFM_CONFIG_DIR', apply_filters( 'wpcfm_config_dir', $config_dir ) );
+define( 'WPCFM_CONFIG_URL', apply_filters( 'wpcfm_config_url', $config_url ) );
