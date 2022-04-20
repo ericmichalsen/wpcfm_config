7aba152ab33e9d67c04e4afde6acff537266328f
Eric Michalsen <81632186+ericmichalsen@users.noreply.github.com>
2022-04-20 15:30:58 -0400

    Update wpcfm_config.php

--- wpcfm_config.php
+++ wpcfm_config.php
@@ -20,6 +20,3 @@ add_filter( 'wpcfm_config_dir', function($dir) {
 
 define( 'WPCFM_CONFIG_DIR', apply_filters( 'wpcfm_config_dir', $config_dir ) );
 define( 'WPCFM_CONFIG_URL', apply_filters( 'wpcfm_config_url', $config_url ) );
-
-
-
