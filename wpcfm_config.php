00d7140088a7e8ab061742af50d3eb96372fafc9
Eric Michalsen <81632186+ericmichalsen@users.noreply.github.com>
2022-04-21 13:52:56 -0400

    Update wpcfm_config.php

--- wpcfm_config.php
+++ wpcfm_config.php
@@ -1,18 +1,16 @@
 <?php
 
-
-if( isset($_ENV['PANTHEON_ENVIRONMENT'] ) {
-	$root = 'DOCROOT';	
+if ( isset($_ENV['DOCUMENT_ROOT']) ) {
+      add_filter( 'wpcfm_config_dir', function($dir) {
+        return $_ENV['DOCUMENT_ROOT'] . '/private/config/dev';
+      }, 10 );
    }
    else {
-	$root= 'DOCUMENT_ROOT';	
+      add_filter( 'wpcfm_config_dir', function($dir) {
+        return $_ENV['DOCROOT'] . '/private/config/dev';
+      }, 10 );
    }
 
-	
-add_filter( 'wpcfm_config_dir', function($dir) {
-	return $_ENV[$root] . '/private/config/dev';
-}, 10 );
-	
 
   $config_dir = untrailingslashit($_ENV[$root]) . '/private/config';
   $config_url = home_url() . '/private/config';
