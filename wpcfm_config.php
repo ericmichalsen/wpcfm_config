9a3f722a4322fbb5e9450ea3126102dbdb87150d
Eric Michalsen <81632186+ericmichalsen@users.noreply.github.com>
2022-04-22 08:51:21 -0400

    Update wpcfm_config.php

--- wpcfm_config.php
+++ wpcfm_config.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( isset($_ENV['DOCUMENT_ROOT']) ) {
+if( isset($_ENV['DOCUMENT_ROOT']) ) {
       add_filter( 'wpcfm_config_dir', function($dir) {
         return $_ENV['DOCUMENT_ROOT'] . '/private/config/dev';
       }, 10 );
@@ -14,6 +14,3 @@ if ( isset($_ENV['DOCUMENT_ROOT']) ) {
 
   $config_dir = untrailingslashit($_ENV[$root]) . '/private/config';
   $config_url = home_url() . '/private/config';
-
-define( 'WPCFM_CONFIG_DIR', apply_filters( 'wpcfm_config_dir', $config_dir ) );
-define( 'WPCFM_CONFIG_URL', apply_filters( 'wpcfm_config_url', $config_url ) );
